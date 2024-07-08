82044e284906ae5ceb20fe4376a16c33e4be3a1f
Artem <artkrasov@gmail.com>
2024-07-08 04:39:25 +0300

    Сменил страницу по умолчанию

--- LibraryForms/Default.aspx.cs
+++ LibraryForms/Default.aspx.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -8,14 +7,9 @@ using System.Web.UI.WebControls;
 
 namespace LibraryForms
 {
-    public partial class Home : System.Web.UI.Page
+    public partial class Default : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void SqlDataSource_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
         {
 
         }
--- LibraryForms/Default.aspx.designer.cs
+++ LibraryForms/Default.aspx.designer.cs
@@ -11,7 +11,7 @@ namespace LibraryForms
 {
 
 
-    public partial class Home
+    public partial class Default
     {
 
         /// <summary>
